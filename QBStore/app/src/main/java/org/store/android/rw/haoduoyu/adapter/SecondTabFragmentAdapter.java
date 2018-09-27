9cc55a583b0501fdfed7a04383ade7f641498f2f
test <test@example.com>
2018-09-27 10:32:31 +0800

    第二个tab-推广界面，

--- QBStore/app/src/main/java/org/store/android/rw/haoduoyu/adapter/SecondTabFragmentAdapter.java
+++ QBStore/app/src/main/java/org/store/android/rw/haoduoyu/adapter/SecondTabFragmentAdapter.java
@@ -2,6 +2,7 @@ package org.store.android.rw.haoduoyu.adapter;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
+
 import org.store.android.rw.haoduoyu.R;
 
 public class SecondTabFragmentAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
@@ -15,6 +16,10 @@ public class SecondTabFragmentAdapter extends BaseQuickAdapter<String, BaseViewH
     @Override
     protected void convert(BaseViewHolder helper, String item) {
         int layoutPosition = helper.getLayoutPosition();
+        Integer i = Integer.valueOf(item);
+        if (i > 0) {
+            item = "+" + item;
+        }
         helper.setText(R.id.tv_item_title, layoutPosition == 0 ? mContext.getResources().getString(R.string.group_list_activity_title) : mContext.getResources().getString(R.string.retail_list_activity_title))
                 .setText(R.id.tv_item_content, layoutPosition == 0 ? "代理是我直接推广的客户" : "零售是代理商帮助我推广的客户")
                 .setText(R.id.tv_item_price, item);
