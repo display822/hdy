05818a15bec89d8edb553e42635e2d6d50fb8f2f
test <test@example.com>
2018-09-27 16:29:00 +0800

    应用图标和 收款方式

--- QBStore/app/src/main/java/org/store/android/rw/haoduoyu/adapter/SecondTabFragmentAdapter.java
+++ QBStore/app/src/main/java/org/store/android/rw/haoduoyu/adapter/SecondTabFragmentAdapter.java
@@ -16,10 +16,6 @@ public class SecondTabFragmentAdapter extends BaseQuickAdapter<String, BaseViewH
     @Override
     protected void convert(BaseViewHolder helper, String item) {
         int layoutPosition = helper.getLayoutPosition();
-        Integer i = Integer.valueOf(item);
-        if (i > 0) {
-            item = "+" + item;
-        }
         helper.setText(R.id.tv_item_title, layoutPosition == 0 ? mContext.getResources().getString(R.string.group_list_activity_title) : mContext.getResources().getString(R.string.retail_list_activity_title))
                 .setText(R.id.tv_item_content, layoutPosition == 0 ? "代理是我直接推广的客户" : "零售是代理商帮助我推广的客户")
                 .setText(R.id.tv_item_price, item);
